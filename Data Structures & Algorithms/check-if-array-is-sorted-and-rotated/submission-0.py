d9285f50adeea7cd83714973a9faa4d11943ae51
Ayaan Saif <138974677+ayaanmsaif@users.noreply.github.com>
2026-08-13 12:21:11 +0100

    Add: check-if-array-is-sorted-and-rotated - submission-0

---
+++ Data Structures & Algorithms/check-if-array-is-sorted-and-rotated/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        
+        comp = nums.copy()
+        comp.sort()
+        print(comp)
+
+        for i in range(1, len(nums)+1):
+            new = [] 
+            
+            for j in range(len(nums)):
+                new.append(nums[(j+i) % len(nums)]) 
+
+            print(new)
+
+            if comp == new:
+                return True 
+
+        return False 
